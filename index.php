fc12bd928b77063b1550036ff78ed64be0febb6f
moe-developerr <moe.developerr@gmail.com>
2016-11-15 14:47:18 +0200

    placeholder fallback 100% working

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script src="js/vendor/jquery-2.1.4.min.js"></script>
+	<script src="js/placeholder.js"></script>
+	<script>
+		$(function () {
+			$.fixPlaceholders()
+		});
+	</script>
+</head>
+<body>
+	<form action="" method="GET">
+		<input type="text" class="input" placeholder="Full name">
+		<input type="text" class="input" placeholder="Age">
+		<input type="text" class="input" placeholder="Job position">
+		<textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+		<input type="submit" value="Submit Form">
+	</form>
+</body>
+</html>
